42fc6f50e6c9e6aa698236df401cdf3d16bfd821
phivanloi <phivanloi@outlook.com>
2022-04-23 01:25:46 +0700

    Update

---
+++ src/Pl.Sas.Core/Entities/Data/CorporateAction.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Pl.Sps.Core.Entities
+{
+    /// <summary>
+    /// Sự kiện quyền
+    /// </summary>
+    public class CorporateAction : BaseEntity
+    {
+        /// <summary>
+        /// Mã chứng khoán
+        /// </summary>
+        public string Symbol { get; set; }
+
+        /// <summary>
+        /// Tên sự kiện
+        /// </summary>
+        public string EventName { get; set; }
+
+        /// <summary>
+        /// Ngày giao dịch không hướng quyền GDKHQ
+        /// </summary>
+        public DateTime ExrightDate { get; set; }
+
+        /// <summary>
+        /// Ngày chốt
+        /// </summary>
+        public DateTime RecordDate { get; set; }
+
+        /// <summary>
+        /// Ngày thực hiện
+        /// </summary>
+        public DateTime IssueDate { get; set; }
+
+        /// <summary>
+        /// Tiêu dề sự kiện
+        /// </summary>
+        public string EventTitle { get; set; }
+
+        /// <summary>
+        /// Ngày phát hành, ngày công bố
+        /// </summary>
+        public DateTime PublicDate { get; set; }
+
+        /// <summary>
+        /// Mã sàn chứng khoán
+        /// </summary>
+        public string Exchange { get; set; }
+
+        /// <summary>
+        /// Danh sách mã sự kiện
+        /// </summary>
+        public string EventListCode { get; set; }
+
+        /// <summary>
+        /// Giá trị
+        /// </summary>
+        public decimal Value { get; set; }
+
+        /// <summary>
+        /// Tỉ lệ
+        /// </summary>
+        public decimal Ratio { get; set; }
+
+        /// <summary>
+        /// Mô tả sự kiện
+        /// </summary>
+        public byte[] ZipDescription { get; set; } = null;
+
+        /// <summary>
+        /// Mã sự kiện
+        /// <para>AGME,AGMR,BALLOT,BCHA,BOME,EGME => Đại hội Đồng Cổ đông</para>
+        /// <para>AIS,NLIS,RETU,SUSP,TS => Niêm yết</para>
+        /// <para>DDALL,DDIND,DDINS,DDRP => GD nội bộ</para>
+        /// <para>DIV,ISS => Trả cổ tức</para>
+        /// <para>KQCT,KQQY,KQSB’ => Kết quả kinh doanh</para>
+        /// <para>AMEN,LIQUI,MA,MOVE,OTHE => Sự kiện khác</para>
+        /// </summary>
+        public string EventCode { get; set; }
+    }
+}
---
+++ src/Pl.Sas.Core/Entities/Data/Stock.cs
@@ -0,0 +1,53 @@
+﻿namespace Pl.Sps.Core.Entities
+{
+    public class Stock : BaseEntity
+    {
+        /// <summary>
+        /// mã giao dịch
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Tên cổ phiếu
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Tên đẩy đủ
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Mô tả
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Sàn niêm yết
+        /// HOSE,HNX,UPCOM
+        /// </summary>
+        public string Exchange { get; set; }
+
+        /// <summary>
+        /// Loại cổ phiếu
+        /// <para>stock => cổ phiếu</para>
+        /// <para>index => các quỹ và chỉ số</para>
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Mã quản lý bên ssi
+        /// </summary>
+        public string SsiStockNo { get; set; }
+
+        /// <summary>
+        /// Tên công ty phát hành
+        /// </summary>
+        public string CompanyName { get; set; }
+
+        /// <summary>
+        /// Tên công ty phát hành bằng tiếng anh
+        /// </summary>
+        public string CompanyNameEn { get; set; }
+    }
+}
