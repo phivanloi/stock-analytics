12a4bf7a2c9c3e63a0d0a55bb25412f1788ad874
phivanloi <phivanloi@outlook.com>
2022-05-13 06:44:08 +0700

    Update

---
+++ src/Pl.Sas.Scheduler/Migrations/SystemDb/20220512234257_InitialCreate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pl.Sas.Scheduler.Migrations.SystemDb
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "KeyValues",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(22)", maxLength: 22, nullable: false),
+                    Key = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KeyValues", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(22)", maxLength: 22, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    DataKey = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    Activated = table.Column<bool>(type: "bit", nullable: false),
+                    ActiveTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsError = table.Column<bool>(type: "bit", nullable: false),
+                    OptionsJson = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KeyValues_Key",
+                table: "KeyValues",
+                column: "Key");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_ActiveTime",
+                table: "Schedules",
+                column: "ActiveTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KeyValues");
+
+            migrationBuilder.DropTable(
+                name: "Schedules");
+        }
+    }
+}
