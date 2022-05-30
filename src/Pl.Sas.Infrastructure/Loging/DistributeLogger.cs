260fb60e532bbe50667ac79d38a3b7c46d0219c0
phivanloi <phivanloi@outlook.com>
2022-05-30 20:48:41 +0700

    Update

--- src/Pl.Sas.Infrastructure/Loging/DistributeLogger.cs
+++ src/Pl.Sas.Infrastructure/Loging/DistributeLogger.cs
@@ -63,7 +63,7 @@ namespace Pl.Sas.Infrastructure.Loging
                 };
                 _httpClient.DefaultRequestHeaders.Add("SecurityKey", _distributeLogOptions.Secret);
             }
-            
+
             _loggingBackgroundTaskQueue.QueueBackgroundWorkItem(async token =>
             {
                 var postObject = new
@@ -74,10 +74,10 @@ namespace Pl.Sas.Infrastructure.Loging
                     Type = (byte)logLevel
                 };
                 await _httpClient.PostAsync($"write", new StringContent(JsonSerializer.Serialize(postObject), Encoding.UTF8, "application/json"), CancellationToken.None);
+                fullMessage = null;
+                message = null;
+                postObject = null;
             });
-
-            fullMessage = null;
-            message = null;
 
             string CreateFullMessage(LogLevel logLevel, EventId eventId, string message, Exception? exception)
             {
