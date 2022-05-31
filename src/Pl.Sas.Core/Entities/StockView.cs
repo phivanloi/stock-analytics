12536ed01aa4887b2653b1646528d02c81859ead
phivanloi <phivanloi@outlook.com>
2022-05-31 08:43:40 +0700

    Update

--- src/Pl.Sas.Core/Entities/StockView.cs
+++ src/Pl.Sas.Core/Entities/StockView.cs
@@ -429,7 +429,7 @@ namespace Pl.Sas.Core.Entities
             return JsonSerializer.Serialize(new
             {
                 smb = Symbol,
-                clcp = LastClosePrice.ShowPrice(),
+                clcp = LastClosePrice.ShowPrice(1),
                 LastOpenPrice,
                 LastHighestPrice,
                 LastLowestPrice,
