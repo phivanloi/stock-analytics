c8acc2f80671d4bd912d6cb9c3120681ef590f73
phivanloi <phivanloi@outlook.com>
2022-06-03 07:14:24 +0700

    Update

--- tests/Pl.Sas.InvestmentPrinciplesTests/Worker.cs
+++ tests/Pl.Sas.InvestmentPrinciplesTests/Worker.cs
@@ -25,7 +25,7 @@ namespace Pl.Sas.InvestmentPrinciplesTests
                 Console.OutputEncoding = Encoding.UTF8;
                 DateTime fromDate = new(2020, 1, 1);
                 DateTime toDate = new(2019, 10, 1);
-                var symbol = "VIX";
+                var symbol = "VND";
                 var chartPrices = (await _chartPriceData.FindAllAsync(symbol)).OrderBy(q => q.TradingDate).ToList();
                 var tradingCharts = chartPrices.Where(q => q.TradingDate >= fromDate).OrderBy(q => q.TradingDate).ToList();
                 var tradingHistory = chartPrices.Where(q => q.TradingDate < fromDate).OrderBy(q => q.TradingDate).ToList();
