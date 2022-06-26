b89a398fcbec68b94a2aaa8e2875a3e5db11f37b
phivanloi <phivanloi@outlook.com>
2022-06-26 10:51:22 +0700

    Update

--- src/Pl.Sas.Core/Trading/ExperimentTrading.cs
+++ src/Pl.Sas.Core/Trading/ExperimentTrading.cs
@@ -23,7 +23,7 @@ namespace Pl.Sas.Core.Trading
 
         public TradingCase Trading(List<ChartPrice> chartPrices, List<ChartPrice> tradingHistory, string exchangeName, bool isNoteTrading = true)
         {
-            tradingCase = new() { IsNote = isNoteTrading, StopLossPercent = -7 };
+            tradingCase = new() { IsNote = isNoteTrading, StopLossPercent = -70 };
 
             foreach (var day in chartPrices)
             {
